11f099c3c8cd25c9cfb1e2b6046d259d3b1202dd
andrew jen <sunny0988398645@gmail.com>
2024-05-02 16:33:49 +0800

    first deploy

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ if __name__=='__main__': #如果以主程式執行
 #將檔案傳到雲端執行:
   #1. 建立runtime.txt檔，內容:python-3.9.0(使用的版本)
   #2. 建立requirements.txt檔，內容:Flask 換行 gunicorn 
-  #3. 建立Procfile，內容:web gunicorn flask(檔名):app(變數名)
+  #3. 建立Procfile，內容:web gunicorn app(檔名):app(變數名)
   #4. 使用命令heroku login登錄，然後git init，再來 heroku git:remote -a python-test-jen(python-test-jen是專案名) ，使用git add . 命令 ，使用git commit -m "First Deploy" ，
